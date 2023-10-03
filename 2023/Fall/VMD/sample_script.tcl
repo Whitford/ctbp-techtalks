708e4c1781eb72b710044414f95355b4bbee0d5d
ed29rice <ed29@rice.edu>
2023-10-03 16:26:41 -0500

    Added extra material

---
+++ 2023/Fall/VMD/sample_script.tcl
@@ -0,0 +1,27 @@
+mol new ./spike_protein.gro 
+mol addfile ./sample_trajectory.xtc 0
+
+source ctbp_basic_scripts/la.tcl
+source ctbp_basic_scripts/orient.tcl
+source ctbp_basic_scripts/visualization.tcl
+source vps.tcl
+
+retrieve_vp 1
+
+mol representation NewCartoon 0.3 10 4.1
+mol material AOChalky
+color Display Background white
+colorByFile 0 ./SASA_scores.txt 2 1 0 2
+
+animate goto 1
+move_vp_render 1 2 0 . move 20
+
+for {set i 1} {$i < 360} {incr i 10} {
+rotate_axis {0 1 0} 10 top
+take_picture
+}
+
+move_vp_render 2 1 20 . move 20
+increment 1 249 5
+
+exit
